159fe8ed5256ba2a32094f1cce173e4093262db7
Rudy Gonzalez <xrgonzalezx@gmail.com>
2017-08-15 18:28:40 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const fs = require('fs');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
-const words = fs
-  .readFileSync('/usr/share/dict/words', 'utf-8')
-  .toLowerCase()
-  .split('\n');
+const words = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n');
 
 
-var randomWord = words[Math.floor(Math.random() * words.length)];
+var randomWord = words[selectRandomWord(0, words.length)];
+if (randomWord.length >= 4 && randomWord <= 6)
+  return
 console.log('randomWord', randomWord);
 var randomWordArray = randomWord.split('');
 //variables used globally
@@ -35,22 +34,31 @@ function replaceDashWithLetter(letter) {
   }
 }
 
+function selectRandomWord(min, max) {
+  return Math.floor(Math.random() * (max - min + 2) + min);
+}
+
+
+
 //set view engine
 app.engine('mustache', mustachExpress());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'mustache');
 
 //middleware
-app.use('/', express.static('./views'));
 app.use('/', express.static('./public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.get('/', function(req, res) {
   if (randomWordArray.toString() === dashedArray.toString()) {
-    return res.render('winnerpage', { randomWord: randomWord });
+    return res.render('winnerpage', {
+      randomWord: randomWord
+    });
   }
   res.render('wordGame', {
-    computerWord: dashedArray,
+    computerWord: dashedArray.join(''),
     guessedLetter: guessedLetter,
     guessCount: guessCount,
     userGuess: userGuesses,
@@ -76,21 +84,21 @@ app.post('/', function(req, res) {
       }
     }
     userGuesses.push(guessedLetter);
-    guessCount -= 1;
+    guessCount -= 0;
   } else if (
     userGuess.guessedLetter.length > 1 ||
     userGuess.guessedLetter.length <= 0 ||
-    userGuess.guessedLetter.indexOf('') >= 0
+    userGuess.guessedLetter.indexOf(' ') >= 0 ||
+    userGuess.guessedLetter >= 0
   ) {
     errorMessage =
       'You may only guess one letter at a time. No spaces. No Numbers';
     return res.redirect('/');
-  } else if (isNaN(userGuesses)) {
+  } else if (userGuesses) {
     guessCount -= 1;
     userGuesses.push(userGuess.guessedLetter);
   }
   if (guessCount <= 0) {
-    guessCount = 8;
     errorMessage = 'Sorry you are out of Guesses';
     return res.render('lostpage', {
       randomWord: randomWord,
@@ -102,7 +110,7 @@ app.post('/', function(req, res) {
 });
 
 app.post('/again', function(req, res) {
-  randomWord = words[Math.floor(Math.random() * words.length)];
+  randomWord = words[selectRandomWord(0, words.length)];
   randomWordArray = randomWord.split('');
   dashedArray = [];
   guessedLetter = '';
@@ -113,11 +121,11 @@ app.post('/again', function(req, res) {
   res.redirect('/');
 });
 
-app.post('/error', function(req, res){
+app.post('/error', function(req, res) {
   errorMessage = '';
   res.redirect('/');
 })
 
-app.listen(3000, function (){
+app.listen(3000, function() {
   console.log(' Game On Gamer');
 });
